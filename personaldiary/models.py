a2698825fcd1be2395530710f4c9c6197efa3dd4
Viktory <mokshan.os@mail.ru>
2025-09-06 10:48:15 +0300

    add branch

--- personaldiary/models.py
+++ personaldiary/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db import models
+from django.conf import settings # Для ссылки на AUTH_USER_MODEL
 
-# Create your models here.
+
+class Moment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='moments')
+    image = models.ImageField(upload_to='moments_photos/')
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Момент пользователя {self.user.username} от {self.created_at.strftime('%Y-%m-%d %H:%M')}"
